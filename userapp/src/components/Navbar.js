ca45220a3f4d7844eb8919073fd099997ab320e6
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-05-28 02:40:24 +0530

    added Navbar

---
+++ userapp/src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <ul>
+                <li><a className="brand">Advertyzement</a></li>
+                <button class="button button1">Get Users</button>
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
